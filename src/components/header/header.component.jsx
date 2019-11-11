b532b2b7e3ad7f6655c47ef0cf10d4f92419acac
Young Woo Jung <youngwoojung@Youngs-MacBook-Pro.local>
2019-11-11 14:24:48 +0900

    add logout func

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,7 +9,8 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser, hidden }) => (
+const Header = ({ currentUser, hidden, setCurrentUser }) => (
+
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
